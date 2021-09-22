d495d13b5d81a00a26843ea7bca2d365e9f08379
Subhi-DaJava <subhyfr809@gmail.com>
2021-09-22 21:01:33 +0200

    Un petit test pour Pull Request, merci

--- Factory/Usine.java
+++ Factory/Usine.java
@@ -1,22 +1,23 @@
 package Factory;
 
+/**
+ * 
+ * Juste pour tester l'envoie de gitHube
+ */
 // Classe usine qui représente un bâtiment capable de construire des unités.
-public class Usine
-{
-    private SimpleFabrique simpleFabrique;// Attribut contenant la fabrique simple.
+public class Usine {
+	private SimpleFabrique simpleFabrique;// Attribut contenant la fabrique simple.
 
-    // Le constructeur permet de sélectionner la fabrique à utiliser.
-    public Usine()
-    {
-        this.simpleFabrique = new SimpleFabrique();
-    }
+	// Le constructeur permet de sélectionner la fabrique à utiliser.
+	public Usine() {
+		this.simpleFabrique = new SimpleFabrique();
+	}
 
-    // Méthode qui permet de construire l'ensemble des unités.
-    public Unite formerUnite(SimpleFabrique.TypeUnite type)
-    {
-        Unite unite = this.simpleFabrique.creerUnite(type);
-        unite.consommerRessource();
-        unite.equiper();
-        return unite;
-    }
+	// Méthode qui permet de construire l'ensemble des unités.
+	public Unite formerUnite(SimpleFabrique.TypeUnite type) {
+		Unite unite = this.simpleFabrique.creerUnite(type);
+		unite.consommerRessource();
+		unite.equiper();
+		return unite;
+	}
 }
